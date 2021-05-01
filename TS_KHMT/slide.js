86aa4ed1b9376aef1f7f9a473a7ac54029396c89
pdanh2682000 <82626385+pdanh2682000@users.noreply.github.com>
2021-05-01 11:08:41 +0700

    Commit version_1
    Niên luận cơ sở KHMT version 1

---
+++ TS_KHMT/slide.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+    $(window).scroll(function () {
+        $(".slideanim-bottom").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+});
